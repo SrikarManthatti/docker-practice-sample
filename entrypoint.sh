8cd5709892aabb921e6f2a7c817dea4d40d401da
SrikarManthatti <srikarmanthatti@gmail.com>
2023-03-27 19:41:35 +0530

    Another clean up

--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "using this so that the arguments are accepted and used"
-
-first_arg="$1"
-second_arg="$2"
-
-python ./kub_sample.py ${first_arg} ${second_arg}
--- scripts/kub_sample.py
+++
@@ -1,5 +0,0 @@
-import sys
-
-v1,v2 = sys.argv[1],sys.argv[2]
-
-print(f"Hello {v1}, {v2}")
